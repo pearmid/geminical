d99259195e9beef82ee25aad5206ac198dd43f09
pearmid <100496394+pearmid@users.noreply.github.com>
2022-03-06 14:43:38 -0500

    new

--- app/screens/CalendarScreen.tsx
+++ app/screens/CalendarScreen.tsx
@@ -1,42 +1,40 @@
 import React from 'react';
 import { View, StyleSheet, FlatList } from 'react-native';
 
-import ListItem from '../components/ListItem';
 import CalendarItem from '../components/CalendarItem';
-import ListItemSeparator from '../components/ListItemSeparator';
-import Icon from '../components/Icon';
 import Screen from '../components/Screen';
-import colors from '../config/colors';
 
 function CalendarScreen(props) {
     const calendarItems = [
-        {
-            _id: 1,
-            event: "Go to work",
-            time: "10:10",
-            description: "Get to work...,"
-        },
-        {
-            _id: 2,
-            event: "Go to bed",
-            time: "20:00",
-            description: "Get to bed...,"
-        },
+                {
+                    _id: 1,
+                    event: "Go to work",
+                    time: "10:10",
+                    description: "Get to work...,",
+                    color: 'primary'
+                },
+                {
+                    _id: 2,
+                    event: "Go to bed",
+                    time: "20:00",
+                    description: "Get to bed...,",
+                    color: 'secondary'
+                },
     ]
     return (
         <Screen>
             <View style={styles.container}>
                 <FlatList 
                     data={calendarItems}
-                    keyExtractor={calendarItem => calendarItem._id.toString()}
-                    ItemSeparatorComponent={ListItemSeparator}
+                    keyExtractor={item => item._id.toString()}
                     renderItem={({ item }) =>
                     <CalendarItem
                         title={item.event}
                         time={item.time}
                         subTitle={item.description}
-
-                    />}
+                        color={item.color}
+                    />
+    }
                 />
             </View>
         </Screen>
